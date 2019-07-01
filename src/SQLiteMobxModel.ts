3225485ccd8bd9102180f8aa31b02bf849549fcc
Carlos Roberto <carlos.parreira@sige.pro.br>
2019-07-01 17:06:14 -0300

    change decorators

--- src/SQLiteMobxModel.ts
+++ src/SQLiteMobxModel.ts
@@ -1,12 +1,12 @@
-import { action, runInAction } from 'mobx'
+import { action, observable, runInAction } from 'mobx'
 import SQLite from './SQLite'
 
 export default abstract class SQLiteMobxModel<T extends { id: number }> {
 	abstract table: string
 	abstract data: T[]
-	abstract where?: string
+	@observable where?: string = ''
 
-	@action
+	@action.bound
 	async loadItems() {
 		const resp = await SQLite.query(
 			`SELECT * FROM ${this.table} WHERE ${this.where || 1}`
@@ -16,36 +16,36 @@ export default abstract class SQLiteMobxModel<T extends { id: number }> {
 		})
 	}
 
-	@action
+	@action.bound
 	async setItems(items: T[]) {
 		await SQLite.truncateTable(this.table)
 		await SQLite.insertMany(this.table, items)
 		await this.loadItems()
 	}
 
-	@action
+	@action.bound
 	async addItems(items: T[]) {
 		await SQLite.insertMany(this.table, items)
 		await this.loadItems()
 	}
 
-	@action
+	@action.bound
 	async addItem(item: T) {
 		await SQLite.insert(this.table, item)
 		await this.loadItems()
 	}
 
-	@action
+	@action.bound
 	async getItem(id: number) {
 		// TODO: implement this
 	}
 
-	@action
+	@action.bound
 	async editItem(item: T) {
 		// TODO: implement this
 	}
 
-	@action
+	@action.bound
 	async removeItem(item: T) {
 		await SQLite.query(`DELETE FROM ${this.table} WHERE id = ${item.id}`)
 		await this.loadItems()
