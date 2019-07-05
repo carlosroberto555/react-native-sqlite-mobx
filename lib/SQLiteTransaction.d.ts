ec03a142538c06b1725ecef074acc755c021c05c
Carlos Roberto <carlos.parreira@sige.pro.br>
2019-07-05 09:39:16 -0300

    remove lib from repo

--- lib/SQLiteTransaction.d.ts
+++
@@ -1,7 +0,0 @@
-import { Transaction } from 'react-native-sqlite-storage';
-import SQLiteResultSet from './SQLiteResultSet';
-export default class SQLiteTransaction {
-    transaction: Transaction;
-    constructor(tx: Transaction);
-    query(statement: string, params?: any[]): Promise<SQLiteResultSet>;
-}
--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as SQLite } from './SQLite';
-export { default as SQLiteMobxModel } from './SQLiteMobxModel';
-export { default as SQLiteMobxFlatList } from './SQLiteMobxFlatList';
