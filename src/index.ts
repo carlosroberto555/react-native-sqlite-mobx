27beb15336324cd524cdf6ff5b8b166f08bf3e03
Carlos Roberto <carlos.parreira@sige.pro.br>
2019-08-12 17:27:09 -0300

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as SQLite } from './SQLite'
 export { default as SQLiteMobxModel } from './SQLiteMobxModel'
 export { default as SQLiteMobxFlatList } from './SQLiteMobxFlatList'
+export { default as SQLiteMobxSchema } from './SQLiteMobxSchema'
